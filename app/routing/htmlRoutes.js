7e899e4bc0643f086a84d337b9dae4a5e6a33a3f
hadiburpee <hadiburpee@gmail.com>
2018-12-06 13:00:53 -0800

    html route works

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,14 @@
-app.get("/survey", function(req, res){
-    
+var path = require("path");
 
-});
+module.exports = function(app) {
+    app.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+
+    });
+}
+
+
+
+
+
+
